6691d704f1621e603f122ec42a8139f2b621b6be
HakuFlaku <57467547+HakuFlaku@users.noreply.github.com>
2023-12-01 19:16:37 -0600

    Full solution, a lot of debug print statements though as I'm still getting used to Python :D

---
+++ Trebuchet.py
@@ -0,0 +1,20 @@
+import re
+
+inputFile = open("input.txt", "r")
+total = 0
+
+for line in inputFile:
+    print(line)
+    nums = re.findall(r'[0-9]', line)
+    print("Individual Numbers", nums)
+    print("First Number", nums[0], ", Last Number", nums[-1])
+    print("Concatenated number", nums[0] + nums[-1])
+    total += int(nums[0] + nums[-1])
+    print("Current total: ", total)
+
+inputFile.close()
+print("Final number: ", total)
+
+outputFile = open("output.txt", "w")
+outputFile.write("Total count: " + str(total))
+outputFile.close()
