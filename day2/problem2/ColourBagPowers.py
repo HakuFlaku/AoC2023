5a8cf0938b16a76fc76ebc77cbf509db0b121ad2
HakuFlaku <57467547+HakuFlaku@users.noreply.github.com>
2023-12-02 12:02:19 -0600

    Problem 2 solution! This one I mostly just ripped the process for problem one where I was already getting the max values of an individual game. With these max values we just need to multiply them for one game, and add it to a total. Bada-bing, bada-boom, we done.

--- day2/problem2/ColourBagPowers.py
+++ day2/problem2/ColourBagPowers.py
@@ -1,23 +1,14 @@
 import os
 
-maxDict = {
-    'red': 12,
-    'green': 13,
-    'blue': 14
-}
-
 def main():
     print("Beginning execution...")
     
     inputFile = open(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'input.txt')), "r")
-    idsTotalValue = 0
+    powerSum = 0
 
     for line in inputFile:
         print("\n*********** LINE PROCESSING START ***********\n")
         print("Processing line: ", line)
-
-        id = int(line.split(':')[0].split(' ')[1])
-        print("Found line id to be: ", id)
 
         rounds = line.strip('\n').split(': ')[1].split('; ')
         print(rounds)
@@ -25,26 +16,16 @@ def main():
         gameMaxValues = getGameMaxValues(rounds)
         print("This games max values are: ", gameMaxValues)
 
-        if(validGame(gameMaxValues)):
-            idsTotalValue += id
-
+        gamePower = 1
+        for value in gameMaxValues.values():
+            gamePower *= value
+        
+        powerSum += gamePower
         print("\n*********** LINE PROCESSING DONE ***********\n")
 
     print("Done processing")
     outputFile = open(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'output.txt')), "w")
-    outputFile.write("Id sum value: " + str(idsTotalValue))
-
-
-
-def validGame(game):
-    valid = True
-
-    print(game)
-    for colour, count in game.items():
-        if(maxDict[colour] < count):
-            valid = False
-
-    return valid
+    outputFile.write("Sum of each games 'power': " + str(powerSum))
 
 def getGameMaxValues(game):
     gameMaxValues = {}
